8aa73505a284881bb16c5f7ae8a06c2346c01e1a
Vitor Mussatto <mussatto@gmail.com>
2017-06-05 22:55:04 +0200

    stream example 1

--- src/main/java/lab/j8/DataFactory.java
+++ src/main/java/lab/j8/DataFactory.java
@@ -2,15 +2,26 @@ package lab.j8;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DataFactory {
 
     public static List<Integer> createListInteger(int maxSize){
+//        List<Integer> myList = new ArrayList<>();
+//        for(int i=0; i<maxSize; i++){
+//            myList.add(i);
+//        }
+
         List<Integer> myList = new ArrayList<>();
-        for(int i=0; i<maxSize; i++){
-            myList.add(i);
-        }
+        IntStream.rangeClosed(0, maxSize).forEach(myList::add);
 
         return myList;
+    }
+
+    public static List<Customer> createCustomers(int maxSize){
+        List<Customer> customers = new ArrayList<>();
+        IntStream.rangeClosed(0, maxSize).forEach( i -> customers.add(new Customer(i, "Customer "+i)));
+
+        return customers;
     }
 }
--- src/test/java/lab/j8/StreamLabTest.java
+++ src/test/java/lab/j8/StreamLabTest.java
@@ -2,6 +2,8 @@ package lab.j8;
 
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +16,60 @@ public class StreamLabTest {
         List<Integer> listTest = DataFactory.createListInteger(10);
         List<Integer> returnedList = listTest.stream().filter(i -> i % 2 ==0).collect(Collectors.toList());
 
+        System.out.println(returnedList);
         assertTrue(returnedList.size() > 0);
         assertTrue(returnedList.size() != 10);
+        assertTrue(returnedList.size() == 6);
         assertTrue(returnedList.get(0) == 0);
         assertTrue(returnedList.get(1) == 2);
         assertTrue(returnedList.get(2) == 4);
+    }
+
+    @Test
+    public void testFilterAndGetCustomers() {
+        List<Customer> customers = DataFactory.createCustomers(10);
+        List<Customer> filteredCustomers = customers.stream().filter(c -> c.getId() % 2 ==0).collect(Collectors.toList());
+
+        assertTrue(filteredCustomers.size() > 0);
+        assertTrue(filteredCustomers.size() != 10);
+        assertTrue(filteredCustomers.size() == 6);
+        assertTrue(filteredCustomers.get(0).getId() == 0);
+        assertTrue(filteredCustomers.get(1).getId() == 2);
+        assertTrue(filteredCustomers.get(2).getId() == 4);
+    }
+
+    @Test
+    public void testFilterAndGetCustomersIds() {
+        List<Customer> customers = DataFactory.createCustomers(10);
+        List<Integer> filteredCustomersId = customers.stream().filter(c -> c.getId() % 2 ==0)
+                .mapToInt(Customer::getId).boxed().collect(Collectors.toList());
+
+        assertTrue(filteredCustomersId.size() > 0);
+        assertTrue(filteredCustomersId.size() != 10);
+        assertTrue(filteredCustomersId.size() == 6);
+        assertTrue(filteredCustomersId.get(0) == 0);
+        assertTrue(filteredCustomersId.get(1) == 2);
+        assertTrue(filteredCustomersId.get(2) == 4);
+    }
+
+    @Test
+    public void testFilterAndGetCustomersIdsParallel() {
+        List<Customer> customers = DataFactory.createCustomers(5000000);
+        Instant e1 = Instant.now();
+        List<Integer> filteredCustomersId = customers.stream().filter(c -> c.getId() % 2 ==0)
+                .mapToInt(Customer::getId).boxed().collect(Collectors.toList());
+        Duration elapsed = Duration.between(e1, Instant.now());
+        System.out.println("elapsed 1:" +elapsed.toString());
+        assertTrue(filteredCustomersId.size() > 0);
+        assertTrue(filteredCustomersId.get(0) == 0);
+        assertTrue(filteredCustomersId.get(1) == 2);
+        assertTrue(filteredCustomersId.get(2) == 4);
+
+        Instant e2 = Instant.now();
+        filteredCustomersId = customers.parallelStream().filter(c -> c.getId() % 2 ==0)
+                .mapToInt(Customer::getId).boxed().collect(Collectors.toList());
+        elapsed = Duration.between(e2, Instant.now());
+
+        System.out.println("elapsed 2:" +elapsed.toString());
     }
 }
